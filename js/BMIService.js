0208f7139eb0fe3164abeeba5ffa5b80fae1465f
Lais <lahh.franco@gmail.com>
2015-05-12 21:21:45 -0300

    added BMIService js

---
+++ js/BMIService.js
@@ -0,0 +1,14 @@
+var BMIService = {
+    
+    calculate : function(height,weight){
+        
+    },
+    
+    description : function(imc){
+        return "to be implemented";
+    }
+    
+    
+    
+    
+}
